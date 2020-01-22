0486269a432750bb6d39e1eae7e4d34f54052e81
Simon Edwardsson <simon.edwardsson@gmail.com>
2020-01-22 21:59:29 +0000

    fingerprint_query support + documentation

--- lib/pg_query.ex
+++ lib/pg_query.ex
@@ -1,14 +1,62 @@
 defmodule PgQuery do
   @moduledoc """
-  Documentation for PgQuery.
+    Collection of method from the libpg_query library.
+
+    The original library can be found at https://github.com/lfittl/libpg_query/tree/10-latest
   """
 
   @on_load :load_nifs 
 
+  @doc false
   def load_nifs() do
     :erlang.load_nif('./priv/query', 0) 
   end
   
+  @doc """
+  Parses one or more sql queries (delimited by `;`), returns a map describing the query.
+
+  ## Examples
+      iex> PgQuery.parse_query("SELECT name from")            
+      {:error, "syntax error at end of input", 17}
+
+      iex> PgQuery.parse_query("SELECT name from users")            
+      {:ok,
+      [
+        %{
+          "RawStmt" => %{
+            "stmt" => %{
+              "SelectStmt" => %{
+                "fromClause" => [
+                  %{
+                    "RangeVar" => %{
+                      "inh" => true,
+                      "location" => 17,
+                      "relname" => "users",
+                      "relpersistence" => "p"
+                    }
+                  }
+                ],
+                "op" => 0,
+                "targetList" => [
+                  %{
+                    "ResTarget" => %{
+                      "location" => 7,
+                      "val" => %{
+                        "ColumnRef" => %{
+                          "fields" => [%{"String" => %{"str" => "name"}}],
+                          "location" => 7
+                        }
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          }
+        }
+      ]}
+  """
+  @spec parse_query(sql :: String.t()) :: {:error, String.t(), number()} | {:ok, list(map())}
   def parse_query(sql) do
     case raw_parse_query(sql) do
       {:ok, json_text} -> Jason.decode(json_text)
@@ -16,7 +64,33 @@ defmodule PgQuery do
     end
   end
 
+  @doc """
+    Same as `parse_query/1` but the result is a json string.
+  """
+  @spec raw_parse_query(sql :: String.t()) :: {:error, String.t()} | {:ok, String.t()}
   def raw_parse_query(_sql) do
+    raise "NIF not loaded"
+  end
+
+  @doc """
+  Returns a fingerprint for a given query, ignoring formatting, values in where clauses etc.
+  
+  Precise rules can be found at https://github.com/lfittl/libpg_query/wiki/Fingerprinting
+  
+  ## Examples
+
+      iex> PgQuery.fingerprint_query("SELECT name from users where id = 1")
+      {:ok, "021ecc4fb237583f0d5612ffc00eadc799741785c0"}
+
+      iex> PgQuery.fingerprint_query("SELECT name from users where id = 2")
+      {:ok, "021ecc4fb237583f0d5612ffc00eadc799741785c0"}
+      
+      iex> PgQuery.fingerprint_query("SELECT age from users where id = 2")
+      {:ok, "02c60fb7b029a0d057ad1ef510ddd66356e4425336"}
+    
+  """
+  @spec fingerprint_query(sql :: String.t()) :: {:error, String.t()} | {:ok, String.t()}
+  def fingerprint_query(_sql) do
     raise "NIF not loaded"
   end
 end
--- src/query.c
+++ src/query.c
@@ -35,6 +35,39 @@ parse_query(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
     }
 }
 
+
+static ERL_NIF_TERM
+fingerprint_query(ErlNifEnv *env, int argc, const ERL_NIF_TERM argv[]) {
+    ErlNifBinary query_bin;
+    PgQueryFingerprintResult result;
+    char *query;
+
+    if (!enif_inspect_iolist_as_binary(env, argv[0], &query_bin)) {
+        return enif_make_badarg(env);
+    }
+
+    query = strndup((char *)query_bin.data, query_bin.size);
+    result = pg_query_fingerprint(query);
+
+    free(query);
+
+    if (result.error) {
+        ErlNifBinary return_bin;
+        return_bin.size = sizeof(char) * strlen(result.error->message);
+        return_bin.data = malloc(return_bin.size);
+        strcpy((char*)return_bin.data, result.error->message);
+        pg_query_free_fingerprint_result(result);
+        return enif_make_tuple3(env, enif_make_atom(env, "error"), enif_make_binary(env, &return_bin), enif_make_int(env, result.error->cursorpos));
+    } else {
+        ErlNifBinary return_bin;
+        return_bin.size = sizeof(char) * strlen(result.hexdigest);
+        return_bin.data = malloc(return_bin.size);
+        strcpy((char*)return_bin.data, result.hexdigest);
+        pg_query_free_fingerprint_result(result);
+        return enif_make_tuple2(env, enif_make_atom(env, "ok"), enif_make_binary(env, &return_bin));
+    }
+}
+
 static int load(ErlNifEnv *env, void **priv, ERL_NIF_TERM info) {
     pg_query_init();
     return 0;
@@ -54,6 +87,7 @@ static void
 unload(ErlNifEnv *env, void *priv) { }
 
 static ErlNifFunc nif_funcs[] = {
-    {"raw_parse_query", 1, parse_query, 0}};
+    {"raw_parse_query", 1, parse_query, 0},
+    {"fingerprint_query", 1, fingerprint_query, 0}};
 
 ERL_NIF_INIT(Elixir.PgQuery, nif_funcs, &load, &reload, &upgrade, &unload)
